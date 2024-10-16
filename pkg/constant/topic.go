8d1a913e5ec6b5c264e6bc5df7f9d9aa05dda3f5
MountQing <13767463+mountqing@user.noreply.gitee.com>
2024-10-16 16:49:22 +0800

    logger init

---
+++ pkg/constant/topic.go
@@ -0,0 +1,3 @@
+package constant
+
+const LOGTOPIC = "log"
--- pkg/logger/init.go
+++ pkg/logger/init.go
@@ -1,48 +1,51 @@
 package logger
 
 import (
+	"github.com/IBM/sarama"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"gopkg.in/natefinch/lumberjack.v2"
-	"os"
-	"path/filepath"
-	"store/pkg/config"
-	"time"
+	"store/pkg/constant"
+	"store/pkg/kafka"
 )
 
-func LoggerInit(conf *config.GlobalConfig) *zap.Logger {
+type Logger struct {
+	producer *kafka.Producer
+	logger   *zap.Logger
+}
+
+func NewLogger(p *kafka.Producer) *Logger {
+	l := new(Logger)
+	l.producer = p
+	l.init()
+	return l
+}
+
+func (l *Logger) init() {
 	logMode := zapcore.DebugLevel
 
 	core := zapcore.NewCore(
 		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
-		getWriteSyncer(conf),
+		zapcore.AddSync(l),
 		logMode,
 	)
-	return zap.New(core)
+	l.logger = zap.New(core)
 }
 
-func getEncoder() zapcore.Encoder {
-	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.TimeKey = "time"
-	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
-	encoderConfig.EncodeTime = func(t time.Time, encoder zapcore.PrimitiveArrayEncoder) {
-		encoder.AppendString(t.Local().Format(time.DateTime))
-	}
-	return zapcore.NewJSONEncoder(encoderConfig)
+func (l *Logger) Write(data []byte) (n int, err error) {
+	l.producer.Publish(&sarama.ProducerMessage{
+		Topic:  constant.LOGTOPIC,
+		Offset: 0,
+		Key:    sarama.StringEncoder("log"),
+		Value:  sarama.ByteEncoder(data),
+	})
+
+	return len(data), nil
 }
 
-func getWriteSyncer(conf *config.GlobalConfig) zapcore.WriteSyncer {
-	stSeparator := string(filepath.Separator)
-	stRootDir, _ := os.Getwd()
-	stLogFilePath := stRootDir + stSeparator + "Logger" + stSeparator + "log" + stSeparator + time.Now().Format(time.DateOnly) + ".txt"
-	//fmt.Println(stLogFilePath)
+func (l *Logger) Info(msg string, fields ...zap.Field) {
+	l.logger.Info(msg, fields...)
+}
 
-	lumberjackSyncer := lumberjack.Logger{
-		Filename:   stLogFilePath,
-		MaxSize:    conf.Logger.MaxSize,
-		MaxBackups: conf.Logger.MaxBackups,
-		MaxAge:     conf.Logger.MaxAge,
-		Compress:   true,
-	}
-	return zapcore.AddSync(&lumberjackSyncer)
+func (l *Logger) Error(msg string, fields ...zap.Field) {
+	l.logger.Error(msg, fields...)
 }
