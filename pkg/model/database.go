b4e675da332d3a76326ac6d59dd68f66e8d06dd6
MountQing <13767463+mountqing@user.noreply.gitee.com>
2024-10-29 08:00:01 +0800

    coding table construction

--- pkg/model/database.go
+++ pkg/model/database.go
@@ -1,10 +1,28 @@
 package model
 
+import "time"
+
 type User struct {
 	ID       string `gorm:"primaryKey"`
 	Account  string
+	Name     string
 	Password string
 	Email    string
+}
+
+type Order struct {
+	ID                 string    `json:"id"`                   // 订单ID
+	UserID             string    `json:"user_id"`              // 用户ID
+	MerchandiseStyleID string    `json:"merchandise_style_id"` // 款式id
+	MerchandiseID      string    `json:"merchandise_id"`       // 商品id
+	TotalAmount        float32   `json:"total_amount"`         // 总金额
+	Status             string    `json:"status"`               // 订单状态
+	PaymentMethod      string    `json:"payment_method"`       // 支付方式
+	ShippingAddress    string    `json:"shipping_address"`     // 收货地址
+	CreatedAt          time.Time `json:"created_at"`           // 下单时间
+	UpdatedAt          time.Time `json:"updated_at"`           // 更新时间
+	Quantity           int       `json:"quantity"`             // 购买数量
+	Price              float32   `json:"price"`                // 单价
 }
 
 type UserRole struct {
--- pkg/model/elasticsearch.go
+++ pkg/model/elasticsearch.go
@@ -1,5 +1,31 @@
 package model
 
+import "time"
+
+// Merchandise 商品
+type Merchandise struct {
+	ID          string   `json:"id"`           // 商品id
+	Name        string   `json:"name"`         // 商品名
+	Info        string   `json:"info"`         // 商品简介
+	PictureList []string `json:"picture_list"` // 照片路径
+	MerchantID  string   `json:"merchant_id"`  // 商家id
+	Delivery    string   `json:"delivery"`     // 快递发货情况
+	Category    string   `json:"category"`     // 商品分类
+}
+
+// MerchandiseStyle 商品款式
+type MerchandiseStyle struct {
+	ID            string    `json:"id"`             // 款式id
+	MerchandiseID string    `json:"merchandise_id"` // 商品id
+	Name          string    `json:"name"`           // 款式名
+	Price         float32   `json:"price"`          // 款式价格
+	Stock         uint      `json:"stock"`          // 款式库存数量
+	Status        string    `json:"status"`         // 款式状态（如在售、下架）
+	CreatedAt     time.Time `json:"created_at"`     // 创建时间
+	UpdatedAt     time.Time `json:"updated_at"`     // 更新时间
+}
+
+// Log 日志
 type Log struct {
 	ID     string `json:"id"`
 	Level  string `json:"level"`
