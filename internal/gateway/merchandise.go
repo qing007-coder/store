838779e378b49aa6bcb72f52e020b57d1e36a9d2
MountQing <13767463+mountqing@user.noreply.gitee.com>
2024-11-04 15:07:48 +0800

    coding merchandise gateway logic

--- internal/gateway/merchandise.go
+++ internal/gateway/merchandise.go
@@ -1,1 +1,34 @@
 package gateway
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"store/internal/proto/merchandise"
+)
+
+type MerchantApi struct {
+	ctx    context.Context
+	srv    *Service
+	client merchandise.MerchandiseService
+}
+
+func NewMerchantApi(srv *Service) *MerchantApi {
+	m := new(MerchantApi)
+	m.init(srv)
+
+	return m
+}
+
+func (m *MerchantApi) init(srv *Service) {
+	m.ctx = context.Background()
+	m.srv = srv
+	m.client = merchandise.NewMerchandiseService("merchant", m.srv.Client())
+}
+
+func (m *MerchantApi) PutAwayMerchandise(ctx *gin.Context) {
+
+}
+
+func (m *MerchantApi) RemoveMerchandise(ctx *gin.Context) {
+
+}
